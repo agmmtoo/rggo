f1bbe1caf8ba04e82bd12e1c22ffc007dab7abed
ammo <solidifyarmor@gmail.com>
2023-11-24 22:35:47 +0630

    feat: preview option

--- ammgo/workingFiles/mdp/main.go
+++ ammgo/workingFiles/mdp/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -25,6 +27,7 @@ const (
 
 func main() {
 	filename := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("s", false, "Skip auto-preview")
 	flag.Parse()
 
 	if *filename == "" {
@@ -32,13 +35,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename, os.Stdout); err != nil {
+	if err := run(*filename, os.Stdout, *skipPreview); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(filename string, out io.Writer) error {
+func run(filename string, out io.Writer, skipPreview bool) error {
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
@@ -57,7 +60,15 @@ func run(filename string, out io.Writer) error {
 	outName := temp.Name()
 	fmt.Fprintln(out, outName)
 
-	return saveHTML(outName, htmlData)
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	return preview(outName)
 }
 
 func parseContent(input []byte) []byte {
@@ -75,4 +86,30 @@ func parseContent(input []byte) []byte {
 
 func saveHTML(outFname string, data []byte) error {
 	return os.WriteFile(outFname, data, 0644)
+}
+
+func preview(fname string) error {
+	cName := ""
+	cParams := []string{}
+
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("OS not supported")
+	}
+
+	cParams = append(cParams, fname)
+
+	cPath, err := exec.LookPath(cName)
+	if err != nil {
+		return err
+	}
+
+	return exec.Command(cPath, cParams...).Run()
 }
