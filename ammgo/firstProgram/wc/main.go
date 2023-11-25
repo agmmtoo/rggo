e0e35baedad5bc5fd4deba2e8a5f5661a313bb17
ammo <solidifyarmor@gmail.com>
2023-11-25 09:17:40 +0630

    wc: process file list

--- ammgo/firstProgram/wc/main.go
+++ ammgo/firstProgram/wc/main.go
@@ -6,13 +6,44 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
+type filenamesValue []string
+
+func (v *filenamesValue) String() string {
+	return fmt.Sprintf("files: %s", strings.Join(*v, ","))
+}
+
+func (v *filenamesValue) Set(value string) error {
+	for _, filename := range strings.Split(value, ",") {
+		*v = append(*v, filename)
+	}
+	return nil
+}
+
 func main() {
+	var files filenamesValue
 	lines := flag.Bool("l", false, "Count lines")
 	bytes := flag.Bool("b", false, "Count bytes")
+	flag.Var(&files, "files", "List of comma separated file names")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines, *bytes))
+
+	if len(files) > 0 {
+		var totalCount int
+		for _, fileName := range files {
+			file, err := os.Open(fileName)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			totalCount += count(file, *lines, *bytes)
+			file.Close()
+		}
+		fmt.Println(totalCount)
+	} else {
+		fmt.Println(count(os.Stdin, *lines, *bytes))
+	}
 }
 
 func count(r io.Reader, countLines bool, countBytes bool) int {
