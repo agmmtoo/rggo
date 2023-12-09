58bd22d55d89d634a8580c217ae0c28d832efc32
ammo <solidifyarmor@gmail.com>
2023-12-09 08:41:58 +0630

    feat: core logic

--- ammgo/processes/goci/main.go
+++ ammgo/processes/goci/main.go
@@ -20,14 +20,14 @@ func main() {
 
 func run(proj string, out io.Writer) error {
 	if proj == "" {
-		return fmt.Errorf("Project directory is required")
+		return fmt.Errorf("Project directory is required: %w", ErrValidation)
 	}
 	args := []string{"build", ".", "errors"}
 
 	cmd := exec.Command("go", args...)
 	cmd.Dir = proj
 	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("'go build' failed: %s", err)
+		return &stepErr{step: "go build", msg: "go build failed", cause: err}
 	}
 	_, err := fmt.Fprintln(out, "Go Build: SUCCESS")
 	return err
