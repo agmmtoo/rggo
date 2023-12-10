48f53d22518d1f29bbb1654c2e8294a5d87a87d7
ammo <solidifyarmor@gmail.com>
2023-12-10 14:46:40 +0630

    feat: bootstrap with cobra

---
+++ ammgo/cobra/pScan/cmd/root.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2023 agmmtoo
+Copyrights apply to this source code.
+Check LICENSE for details.
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "pScan",
+	Short: "Fast TCP port scanner",
+	Long: `pScan - short for Port Scanner - executes TCP port scan on a list of hosts.
+
+pScan allows you to add, list, and delete hosts from the list.
+
+pScan executes a port scan on specified TCP ports. You can customize the
+target ports using a command line flag.`,
+	Version: "0.1",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// cobra.OnInitialize(initConfig)
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pScan.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
+}
---
+++ ammgo/cobra/pScan/main.go
@@ -0,0 +1,13 @@
+/*
+Copyright © 2023 agmmtoo
+Copyrights apply to this source code.
+Check LICENSE for details.
+
+*/
+package main
+
+import "agmmtoo.me/ammgo/cobra/pScan/cmd"
+
+func main() {
+	cmd.Execute()
+}
