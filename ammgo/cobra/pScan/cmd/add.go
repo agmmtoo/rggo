6a62e6ec66fb2b315c6e69bf6a8737a3453546d4
ammo <solidifyarmor@gmail.com>
2023-12-12 22:59:38 +0630

    feat: add subcommand

--- ammgo/cobra/pScan/cmd/add.go
+++ ammgo/cobra/pScan/cmd/add.go
@@ -2,41 +2,48 @@
 Copyright © 2023 agmmtoo
 Copyrights apply to this source code.
 Check LICENSE for details.
-
 */
 package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
+	"agmmtoo.me/ammgo/cobra/pScan/scan"
 	"github.com/spf13/cobra"
 )
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+	Use:          "add <host1>...<hostn>",
+	Aliases:      []string{"a"},
+	Short:        "Add new host(s) to list",
+	Args:         cobra.MinimumNArgs(1),
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+		return addAction(os.Stdout, hostsFile, args)
 	},
 }
 
 func init() {
 	hostsCmd.AddCommand(addCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func addAction(out io.Writer, hostsFile string, args []string) error {
+	hl := &scan.HostsList{}
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+	for _, h := range args {
+		if err := hl.Add(h); err != nil {
+			return err
+		}
+		fmt.Fprintln(out, "Added host:", h)
+	}
+	return hl.Save(hostsFile)
 }
