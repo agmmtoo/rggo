c4fc3df676376497e2fd50de40e9895661417de6
ammo <solidifyarmor@gmail.com>
2023-12-23 00:25:40 +0630

    feat: init todo handlers

--- ammgo/apis/todoServer/handlers.go
+++ ammgo/apis/todoServer/handlers.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"sync"
+
+	"agmmtoo.me/ammgo/interacting/todo"
+)
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -9,4 +15,33 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	content := "There's an API here"
 	replyTextContent(w, r, http.StatusOK, content)
+}
+
+var (
+	ErrNotFound    = errors.New("not found")
+	ErrInvalidData = errors.New("invalid data")
+)
+
+func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		list := &todo.List{}
+		l.Lock()
+		defer l.Unlock()
+		if err := list.Get(todoFile); err != nil {
+			replyError(w, r, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if r.URL.Path == "" {
+			switch r.Method {
+			case http.MethodGet:
+				getAllHandler(w, r, list)
+			case http.MethodPost:
+				addHandler(w, r, list, todoFile)
+			default:
+				message := "Method not supported"
+				replyError(w, r, http.StatusMethodNotAllowed, message)
+			}
+		}
+	}
 }
--- ammgo/apis/todoServer/server.go
+++ ammgo/apis/todoServer/server.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func newMux(todoFile string) http.Handler {
 	m := http.NewServeMux()
@@ -12,4 +15,20 @@ func replyTextContent(w http.ResponseWriter, r *http.Request, code int, content 
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(code)
 	w.Write([]byte(content))
+}
+
+func replyJSONContent(w http.ResponseWriter, r *http.Request, status int, resp *todoResponse) {
+	body, err := resp.MarshalJSON()
+	if err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(body)
+}
+
+func replyError(w http.ResponseWriter, r *http.Request, status int, message string) {
+	log.Printf("%s %s: Error: %d %s", r.URL, r.Method, status, message)
+	http.Error(w, http.StatusText(status), status)
 }
