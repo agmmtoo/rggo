fb840376c30761d520f34522c6b57430db2a9e23
ammo <solidifyarmor@gmail.com>
2023-12-24 21:43:32 +0630

    feat: get all func

--- ammgo/apis/todoClient/cmd/client.go
+++ ammgo/apis/todoClient/cmd/client.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
 )
 
 var (
@@ -10,3 +15,56 @@ var (
 	ErrInvalidResponse = errors.New("Invalid server response")
 	ErrNotNumber       = errors.New("Not a number")
 )
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type response struct {
+	Results      []item `json:"results"`
+	Date         int    `json:"date"`
+	TotalResults int    `json:"total_results"`
+}
+
+func newClient() *http.Client {
+	c := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	return c
+}
+
+func getItems(url string) ([]item, error) {
+	r, err := newClient().Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrConnection, err)
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != http.StatusOK {
+		msg, err := io.ReadAll(r.Body)
+		if err != nil {
+			return nil, fmt.Errorf("Cannot read body: %w", err)
+		}
+		err = ErrInvalidResponse
+		if r.StatusCode == http.StatusNotFound {
+			err = ErrNotFound
+		}
+		return nil, fmt.Errorf("%w: %s", err, msg)
+	}
+	var resp response
+	if err := json.NewDecoder(r.Body).Decode(&resp); err != nil {
+		return nil, err
+	}
+	if resp.TotalResults == 0 {
+		return nil, fmt.Errorf("%w: No results found", ErrNotFound)
+	}
+	return resp.Results, nil
+}
+
+func getAll(apiRoot string) ([]item, error) {
+	u := fmt.Sprintf("%s/todo", apiRoot)
+	return getItems(u)
+}
---
+++ ammgo/apis/todoClient/cmd/list.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2023 agmmtoo
+Copyrights apply to this source code.
+Check LICENSE for details.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List todo items",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		apiRoot := viper.GetString("api-root")
+		return listAction(os.Stdout, apiRoot)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
+
+func listAction(out io.Writer, apiRoot string) error {
+	items, err := getAll(apiRoot)
+	if err != nil {
+		return err
+	}
+	return printAll(out, items)
+}
+
+func printAll(out io.Writer, items []item) error {
+	w := tabwriter.NewWriter(out, 3, 2, 0, ' ', 0)
+	for k, v := range items {
+		done := "-"
+		if v.Done {
+			done = "X"
+		}
+		fmt.Fprintf(w, "%s\t%d\t%s\t\n", done, k+1, v.Task)
+	}
+	return w.Flush()
+}
